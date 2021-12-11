073f97f6072b56a4224441b53fd8a8c26e5a922a
liorshar <liorshar@post.bgu.ac.il>
2021-12-11 19:36:29 +0200

    ex7

--- app.py
+++ app.py
@@ -2,19 +2,18 @@ from flask import Flask, redirect, url_for
 
 app = Flask(__name__)
 
-
 @app.route('/')
 def print_hello():
     return redirect('/hello')
+
+@app.route('/hello')
+def hello_func():
+    return 'HELLO!'
 
 @app.route('/home')
 def call_hello_def():
     return redirect(url_for('hello_func'))
 
-
-@app.route('/hello')
-def hello_func():  # put application's code here
-    return 'HELLO!'
 
 if __name__ == '__main__':
     app.run(debug=True)
